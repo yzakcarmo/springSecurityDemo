25111f10cd0685ddcc3eb9567551bc96fb30d74a
Yzak Carmo <yzak.carmo@gmail.com>
2024-09-04 21:31:34 -0300

    feat(security): Configurando o Repository e adicionando o AuthorizationService

--- src/main/java/com/yzakcarmo/springsecuritydemo/repositories/UserRepository.java
+++ src/main/java/com/yzakcarmo/springsecuritydemo/repositories/UserRepository.java
@@ -2,10 +2,12 @@ package com.yzakcarmo.springsecuritydemo.repositories;
 
 import com.yzakcarmo.springsecuritydemo.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.List;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+    UserDetails findByEmail(String email);
     List<User> findByNameContainingIgnoreCase(String name);
     List<User> getAllByRole(Integer role);
 }
